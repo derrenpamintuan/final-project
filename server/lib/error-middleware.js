d7958387cabb60274fb47e043a1a8119d3b75415
Derren Pamintuan <pamintuan2000@gmail.com>
2024-03-06 01:49:11 +0000

    updated error-middleware.js

--- server/lib/error-middleware.js
+++ server/lib/error-middleware.js
@@ -5,7 +5,7 @@ export default function errorMiddleware(err, req, res, next) {
   if (err instanceof ClientError) {
     res.status(err.status).json({ error: err.message });
   } else if (err instanceof jwt.JsonWebTokenError) {
-    res.status(401).json({ error: 'invalid access token' });
+    res.status(401).json({ error: 'invalid access token given' });
   } else {
     console.error(err);
     res.status(500).json({ error: 'an unexpected error occurred' });
